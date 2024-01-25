432b2ee15d171cac8888cf1f6273a01e52e014cf
Lucas Santos <ls6865316@gmail.com>
2024-01-25 13:36:43 -0300

    publishing game

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,28 @@
+const elementoChute = document.querySelector('#chute')
+
+window.SpeechRecognition = window.SpeechRecongnition || webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+// Quando falar no audio a linguagem entendida vai ser o portugues
+recognition.lang = 'pt-Br'
+
+recognition.start();
+
+/* Quando recognition comecar pega o resultado dele*/
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(event) {
+  chute = event.results[0][0].transcript
+  exibeChuteNaTela(chute)
+  verificaSeOChutePossuiUmValorValido(chute)
+}
+
+function exibeChuteNaTela(chute) {
+  elementoChute.innerHTML = `
+  <div>Voce disse:</div>
+  <span class="box">${chute}</span>
+  `
+}
+
+// Depois de falar um numero e encerrar, liga mais uma vez o reconhecimento de voz para continuar
+recognition.addEventListener('end', () => recognition.start())
---
+++ app/sortearNumero.js
@@ -0,0 +1,13 @@
+const menorValor = 1
+const maiorValor = 1000
+const numeroSecreto = gerarNumeroAleatorio()
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * maiorValor + 1) 
+}
+
+const elementoMenorValor = document.querySelector('#menor-valor')
+const elementoMaiorValor = document.querySelector('#maior-valor')
+
+elementoMenorValor.innerHTML = menorValor
+elementoMaiorValor.innerHTML = maiorValor
